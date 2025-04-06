229a0f6f25a34452a368c377804b86413e0902ee
Seth Porter <symmetry@pobox.com>
2025-04-06 15:57:34 -0400

    Made cert-manager install retryable. Working for TLS after some iterations.

--- cert-manager/install.sh
+++ cert-manager/install.sh
@@ -3,11 +3,22 @@ set -euxo pipefail
 OUT_DIR=`pwd`
 pushd "$(dirname "$0")"
 SAVE_DIR=`pwd`
+
+helm dep update
 popd
 
-kubectl create namespace cert-manager
-  && kubectl label namespace cert-manager pod-security.kubernetes.io/warn=baseline
-kubectl create secret generic clouddns-sa --namespace cert-manager \
-   --from-literal=key.json="$(op read  op://tales-secrets/cert-manager/clouddns-sa/notesPlain)"
-helm install --namespace cert-manager cert-manager "${SAVE_DIR}/cert-manager"
+if  ! kubectl get namespace cert-manager ; then
+  kubectl create namespace cert-manager \
+    && kubectl label namespace cert-manager pod-security.kubernetes.io/warn=baseline
+fi
+
+# If not logged in, op read b
+eval $(op signin)
+
+if ! kubectl get secret clouddns-sa --namespace cert-manager ; then
+  kubectl create secret generic clouddns-sa --namespace cert-manager \
+    --from-literal=key.json="$(op read  op://tales-secrets/cert-manager-clouddns-sa/notesPlain)"
+fi
+
+helm upgrade --install --namespace cert-manager cert-manager "${SAVE_DIR}"
 kubectl apply -f "${SAVE_DIR}/issuers.yaml"

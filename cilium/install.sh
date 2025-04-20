702f4a56da8744c49142d53d1c59b43bc02fe5db
Seth Porter <symmetry@pobox.com>
2025-04-20 09:01:56 -0400

    Way back in time fingers crossed

--- cilium/install.sh
+++ cilium/install.sh
@@ -15,16 +15,11 @@ kubectl apply -f https://github.com/kubernetes-sigs/gateway-api/releases/downloa
 # `enforce` is what it actually allows you to do, but `warn` will log a warning that looks awfully fatal (at least to me) and send you off on
 # a wild goose chase, so we disable both.
 
-if  ! kubectl get namespace cilium ; then
-  kubectl create namespace cilium \
-    && kubectl label namespace cilium pod-security.kubernetes.io/enforce=privileged \
-    && kubectl label namespace cilium pod-security.kubernetes.io/warn=privileged
-fi
+kubectl create namespace cilium \
+  && kubectl label namespace cilium pod-security.kubernetes.io/enforce=privileged \
+  && kubectl label namespace cilium pod-security.kubernetes.io/warn=privileged
 
-# helm upgrade --install --namespace cilium cilium "${SAVE_DIR}"
-helm template --namespace cilium cilium "${SAVE_DIR}" > ${SAVE_DIR}/manifest.yaml
-kubectl apply -f ${SAVE_DIR}/manifest.yaml --server-side
-rm ${SAVE_DIR}/manifest.yaml
+helm upgrade --install --namespace cilium cilium "${SAVE_DIR}"
 echo "Wait for CRDs to become available"
 sleep 60
-kubectl apply -f "${SAVE_DIR}/additional-manifests.yaml" --server-side
+kubectl apply -f "${SAVE_DIR}/additional-manifests.yaml"

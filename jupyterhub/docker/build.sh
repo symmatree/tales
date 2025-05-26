b931b964129bc82e21c36f7939a36b8154e57c4f
Seth Porter <symmetry@pobox.com>
2025-05-26 12:58:45 -0400

    switch to systemctl replacement

--- jupyterhub/docker/build.sh
+++ jupyterhub/docker/build.sh
@@ -7,11 +7,20 @@ SAVE_DIR=`pwd`
 TOKEN=`op read op://tales-secrets/jupyterhub-github-token/password`
 REPOUSER=`op read op://tales-secrets/jupyterhub-github-token/username`
 IMAGE=ghcr.io/symmatree/internal/datascience-notebook-ssh
-TAG=$(date +%Y-%m-%d)
+TAG=$(date +%Y-%m-%d)-2
+
+BASE_IMAGE=quay.io/jupyter/datascience-notebook
+# https://quay.io/repository/jupyter/datascience-notebook?tab=tags&tag=latest
+BASE_TAG=2025-05-24
+
+# Pull first to avoid collision on creds.
+buildah pull $BASE_IMAGE:$BASE_TAG
 buildah build --network=host \
   --layers \
   --disable-compression=false \
   --creds "$REPOUSER:$TOKEN" \
+  --build-arg BASE_IMAGE=$BASE_IMAGE \
+  --build-arg BASE_TAG=$BASE_TAG \
   --cache-from ${IMAGE}-cache \
   --cache-to ${IMAGE}-cache \
   --tag $IMAGE:$TAG $SAVE_DIR

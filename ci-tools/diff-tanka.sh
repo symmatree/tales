0cc4f92ba1edcd750390ff7381ad8f24c668657e
Seth Porter <symmetry@pobox.com>
2025-07-19 15:36:59 +0000

    Fix diff-tanka.sh

--- ci-tools/diff-tanka.sh
+++ ci-tools/diff-tanka.sh
@@ -13,20 +13,25 @@ for dir in "${TANKA[@]}"; do
 	echo "$dir"
 	cd "$dir"
 
-	NAMESPACE=$(basename "$dir")
-	echo "Namespace: ${NAMESPACE} (${dir})"
-	kubectl config set-context --current --namespace="$NAMESPACE"
-	DIFFS=$(tk show --dangerous-allow-redirect \
-		environments/default |
-		kubectl diff -n "${NAMESPACE}" --server-side=true -f - --force-conflicts=true) || true
-	if [ -n "$DIFFS" ]; then
-		echo "::notice file=${dir},title=${NAMESPACE}-Diffs::${DIFFS}"
-		#echo "Differences found in $dir:"
-		#echo "$DIFFS"
-		# echo "::endgroup::"
-	else
-		echo "No differences found in $dir."
-	fi
+	for env in environments/*; do
+		echo "Validating environment: $env"
+
+		NAMESPACE=$(basename "$env")
+		echo "Namespace: ${NAMESPACE} (${dir}/${env})"
+		kubectl config set-context --current --namespace="$NAMESPACE"
+		DIFFS=$(tk show --dangerous-allow-redirect "$env" |
+			kubectl diff -n "${NAMESPACE}" --server-side=true -f - --force-conflicts=true) || true
+		if [ -n "$DIFFS" ]; then
+			# echo "::notice file=${dir},env=${env},title=${NAMESPACE}-Diffs::${DIFFS}"
+			echo "::group::${dir} ${env} title=${NAMESPACE}"
+			echo "file=${dir},env=${env},title=${NAMESPACE}-Diffs::${DIFFS}"
+			echo "$DIFFS"
+			# echo "::endgroup::"
+		else
+			echo "No differences found in $env."
+		fi
+	done
+
 	echo "end $dir"
 done
 echo "All Tanka environments diffed successfully."

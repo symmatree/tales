248045c532afdc2725f524ae8b9484ff05d53e5d
symmatree <symmetry@pobox.com>
2025-08-10 19:07:35 -0400

    Maybe fix OTLP (#117)
    * Implement config from https://github.com/grafana/k8s-monitoring-helm/blob/main/charts/k8s-monitoring/docs/Collectors.md\#alloy-receiver
    
    * Make it a deployment
    
    * And try to disable exporting
    
    * Meh make it work first
    
    * Add yq before I forget
    
    * Fix helm-add-repos

--- ci-tools/helm-add-repos.sh
+++ ci-tools/helm-add-repos.sh
@@ -5,12 +5,14 @@ CI_TOOLS=$(pwd)
 WORKSPACE=$(dirname "$CI_TOOLS")
 echo "WORKSPACE: $WORKSPACE"
 cd "${WORKSPACE}"
-mapfile -t urls < <(find . -name "Chart.yaml" -exec yq ".dependencies[] | .repository" "{}" ';' | sort | uniq)
+mapfile -t urls < <(find . -name "Chart.yaml" -exec yq --raw-output ".dependencies[] | .repository" "{}" ';' | sort | uniq)
 repo_num=1
 for url in "${urls[@]}"; do
 	if ! helm repo list | grep -q "$url"; then
 		echo "Adding Helm repository: $url"
 		helm repo add "$(basename "$url")-${repo_num}" "$url"
 		((++repo_num))
+	else
+		echo "Found Helm repo for url, skipping: $url"
 	fi
 done
